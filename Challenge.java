c0bb3f118a54c9884aa6bc16708bd0283fdbfe42
teachable <87560308+teachable@users.noreply.github.com>
2021-07-27 23:27:58 +0530

    Updating

---
+++ Challenge.java
@@ -0,0 +1,19 @@
+class Challenge {
+	public String day, month, year;
+	
+	public Challenge(String Challenge) {
+		
+		//write down your code here
+        //start
+
+	
+
+        //end
+	}
+    public static void main(String[] arguments) {
+        Challenge birth = new Challenge("04/29/2016");
+        System.out.println("Month: " + birth.month);
+        System.out.println("Day: " + birth.day);
+        System.out.println("Year: " + birth.year);    
+    }
+}
